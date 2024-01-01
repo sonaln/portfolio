43e6f799fa620b41a3d13c2c2d6d34c6441d086f
sonaln <sonal.sachin.naik@gmail.com>
2024-01-01 00:17:16 -0800

    moved config

---
+++ src/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"oUhtP3PyhayEB97V1NcLooxobMYnC27VgByk8RBsFv4=\"}"
--- src/app/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./page"
-export default function Index(){
-  return(
-    <Home/>
-  );
-}
