9b8d4eae340e3508195edf425c55e4dfab61a335
sonaln <sonal.sachin.naik@gmail.com>
2024-01-01 14:05:32 -0800

    added resume href button

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub, AiFillMail} from 'react-icons/ai'
+import { FaGalacticRepublic } from "react-icons/fa6";
 import { useState } from 'react'
 
 
@@ -14,18 +15,18 @@ export default function Home() {
 
     <div className={darkMode ? "dark" : ""}>
     <Head>
-      <title>Sonal Naik Portfolio</title>
+      <title>Sonal Naik\'s Portfolio</title>
       <meta name="description" content="General"/>
-      <link rel="icon" href="/favicon.ico"/>
+      <link rel="icon" href={<FaGalacticRepublic/>}/>
     </Head>
 
     <main className='bg-white px-10 md:px-20 lg:px-30 xl:px-40 dark:bg-black'>
       <section className="min-h-screen">
         <nav className='p-10 mb-12 flex justify-between'>
-          <h1 className='text-xl'>Sonal Naik</h1>
-          <ul className='flex items-center'>
+          <h1 className='text-xl'>Sonal's Portfolio</h1>
+          <ul className='flex items-center gap-5'>
             <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/></li>
-            <li><a className='bg-cyan-600 text-white px-5 rounded-md'href='#'>Resume</a></li>
+            <li><a className='bg-cyan-600 text-white px-5 rounded-md'href='#' onClick={href => window.open("https://www.google.com", "_blank")}>Resume</a></li>
           </ul>
         </nav>
 
@@ -33,19 +34,20 @@ export default function Home() {
 
         </div>
         <div className='text-center p-10'>
-          <h2 className='text-5xl py-2 text-teal-500 font-medium'>
+          <h2 className='text-6xl py-2 text-teal-500 font-medium'>
             Sonal Naik
           </h2>
           <h3 className='text-2xl py-2'>Aspiring Software Developer</h3>
           <p>
+            Hello! My name is Sonal Naik
 
           </p>
         </div>
 
         <div className='text-5xl flex justify-center gap-16'>
-          <AiFillMail />
-          <AiFillLinkedin />
-          <AiFillGithub />
+          <AiFillMail onClick={href => window.open('mailto:sonal.sachin.naik@gmail.com', '_blank')}/>
+          <AiFillLinkedin onClick={href => window.open("https://www.linkedin.com/in/sonaln", '_blank')}/>
+          <AiFillGithub onClick={href => window.open('https://www.github.com/sonaln', '_blank')}/>
 
         </div>
       </section>
