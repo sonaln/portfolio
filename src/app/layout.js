9f018e410dc70e2396f56493693895a584599627
sonaln <sonal.sachin.naik@gmail.com>
2024-01-03 15:53:26 -0800

    added tracker

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Analytics from '@vercel/analytics/react'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,13 +7,16 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Sonal Naik',
   description: 'Sonal Naik\'s Portfolio',
-  
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} 
+        <Analytics />
+      </body>
     </html>
   )
 }
