d6d1342ae67a1f56ca6d594fcbe9e0a1590009fc
sonaln <sonal.sachin.naik@gmail.com>
2024-01-03 16:09:11 -0800

    added analytics tracker:

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import Analytics from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Sonal Portfolio</title>
+      </head>
       <body className={inter.className}>
         {children} 
         <Analytics />
